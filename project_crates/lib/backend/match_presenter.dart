fe618b2d458eada603c2a6efb60e200df9610a75
Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2021-04-03 23:16:14 +0800

    Partially completed matching

--- project_crates/lib/backend/match_presenter.dart
+++ project_crates/lib/backend/match_presenter.dart
@@ -4,8 +4,7 @@ import 'package:flutter_application_1/models/Listing.dart';
 import 'package:flutter_application_1/models/ListingImageData.dart';
 import 'package:http/http.dart' as http;
 
-class MatchPresenter{
-
+class MatchPresenter {
   final _databaseRef = FirebaseDatabase.instance.reference();
   String url = "api.foodai.org";
 
@@ -23,72 +22,105 @@ class MatchPresenter{
       }),
     );
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       //print(response.body);
       var responseJson = json.decode(response.body);
       var categories = responseJson['food_results_by_category'];
       var length = responseJson['food_results_by_category'].length;
 
-      for(int i = 0; i < length; i++){
-        map[categories[i][0]] = double.parse(categories[i][1]);
+      for (int i = 0; i < length; i++) {
+        var weight = double.parse(categories[i][1]);
+        // don't store if the weight is not at least 0.7
+        if (weight >= 0.7) {
+          map[categories[i][0]] = weight;
+        }
       }
     }
     //If no response == 200, means no match
-    else{
-    }
+    else {}
 
     return map;
   }
 
-  Future addListingImageData(ListingImageData data) async{
+  Future addListingImageData(ListingImageData data) async {
     await _databaseRef.child("ListingImageData").push().set({
       "listingID": data.listingID,
       "categories": data.categories,
     });
   }
 
+  Future<List<String>> getMatchedListingIDs(
+      Map<String, double> categories) async {
+    // to store all LIDs
+    var listingImageDataList = [];
 
-  Future<List<String>> getMatchedListingIDs(List<String> categories) async{
     // list of listings IDs that has common classifications
-    var matchedListingIDs = [];
+    var results = <String>[];
 
-    // retrieve a list of listing IDs that has common classifications
-    try{
-      await _databaseRef.child('ListingImageData').once().then((DataSnapshot snapshot) {
+    // retrieve all ListingImageData from db
+    try {
+      await _databaseRef
+          .child('ListingImageData')
+          .once()
+          .then((DataSnapshot snapshot) {
         Map<dynamic, dynamic> map = snapshot.value;
         map.forEach((key, value) {
-          // at least one common classification category
-          if (categories.any((item) => value['categories'].contains(item))) {
-            matchedListingIDs.add(value['listingID']);
-          }
+          var lid = ListingImageData(
+            listingID: value['listingID'],
+            categories: Map.from(value['categories']),
+          );
+          listingImageDataList.add(lid);
         });
       });
-      print('Listing IDs that matched classification category "$categories": $matchedListingIDs');
-    }catch(e){
-      print('getMatchedListings(): Error occurred retrieving ListingImageData model');
-    };
+      print(
+          'Retrieved ${listingImageDataList.length} listingImageData (LIDs) from db!');
+    } catch (e) {
+      print(e);
+      print(
+          'getMatchedListings(): Error occurred retrieving ListingImageData model');
+      return null;
+    }
+    ;
 
-    return matchedListingIDs;
+    // sort
+    var sortedCategories = categories.keys.toList()
+      ..sort((a, b) => categories[b].compareTo(categories[a]));
+
+    // loop through each category
+    sortedCategories.forEach((key) {
+      listingImageDataList.forEach((lid) {
+        if (lid.categories.containsKey(key)) {
+          results.add(lid.listingID);
+        }
+      });
+    });
+    print('Matched ${results.length} LIDS');
+    return results;
   }
-
 
   // query the db for listings that match
   Future<List<Listing>> getMatchedListings(List<String> matchedListingIDs) async {
     // list of listings to store the result
-    var matchedListings = [];
+    var matchedListings = <Listing>[];
+
     // retrieves all listings, and do matching
-    try{
-      await _databaseRef.child('Listing').orderByChild('postDateTime').once().then((DataSnapshot snapshot) {
+    try {
+      await _databaseRef
+          .child('Listing')
+          .orderByChild('postDateTime')
+          .once()
+          .then((DataSnapshot snapshot) {
         Map<dynamic, dynamic> map = snapshot.value;
-        map.forEach((key, value) {
+
+        for (dynamic key in map.keys){
+          var value = map[key];
 
           // if listing is not complete, and it's ID is in the matchedListingIDs list
-          if (value['isComplete'] == false && matchedListings.contains(key)){
-
+          if (value['isComplete'] == false && matchedListingIDs.contains(key)) {
             // construct Listing object
             var match = Listing(
                 listingID: key,
-                userID : value['userID'],
+                userID: value['userID'],
                 listingTitle: value['listingTitle'],
                 category: value['category'],
                 postDateTime: DateTime.parse(value['postDateTime']),
@@ -101,16 +133,38 @@ class MatchPresenter{
 
             // append to results list
             matchedListings.add(match);
+
+            // if 3 already found, break early
+            if (matchedListings.length >= 3){
+              break;
+            }
+
           }
-          });
-        });
-    }catch(e){
+        }
+      });
+    } catch (e) {
       print('getMatchedListings(): Error occurred retrieving matches');
-    };
+    }
+    ;
     print('Matched: ${matchedListings.length} listings');
     return matchedListings;
   }
 
+  Future<List<Listing>> getMatches(Map<String, double> classifications) async{
+    var matchedListingsID = await getMatchedListingIDs(classifications);
+
+    if (matchedListingsID.isEmpty){
+      return null;
+    }
+
+    var matchedListings = await getMatchedListings(matchedListingsID);
+
+    if (matchedListings.isEmpty){
+      return null;
+    }
+
+    return matchedListings;
+  }
+
+
 }
-
-
--- project_crates/lib/screens/listing/Newlisting_page.dart
+++ project_crates/lib/screens/listing/Newlisting_page.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/backend/databaseAccess.dart';
+import 'package:flutter_application_1/backend/match_presenter.dart';
 import 'package:flutter_application_1/backend/storageAccess.dart';
 import 'package:flutter_application_1/models/Listing.dart';
+import 'package:flutter_application_1/models/ListingImageData.dart';
+import 'package:flutter_application_1/screens/common/error_popup_widgets.dart';
 import 'package:flutter_application_1/screens/common/user_main.dart';
 import 'package:flutter_application_1/screens/nearby/nearby_MapHandler.dart';
 import 'package:flutter_google_places/flutter_google_places.dart';
@@ -23,7 +26,7 @@ class Newlisting_page extends StatelessWidget {
             backgroundColor: Color(0xFFFFC857),
             shape: RoundedRectangleBorder(
                 borderRadius:
-                BorderRadius.vertical(bottom: Radius.circular(70.0))),
+                    BorderRadius.vertical(bottom: Radius.circular(20.0))),
             title: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -49,8 +52,6 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  //String isSelected;
-
   @override
   initState() {
     super.initState();
@@ -90,9 +91,8 @@ class _BodyState extends State<Body> {
     return Scaffold(
       body: SingleChildScrollView(
         //body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
           SizedBox(height: 20),
           InkWell(
             onTap: () async {
@@ -103,22 +103,22 @@ class _BodyState extends State<Body> {
                 borderRadius: BorderRadius.circular(20.0),
                 child: image != null
                     ? Image.file(
-                  image,
-                  height: 200,
-                  width: 200,
-                )
+                        image,
+                        height: 200,
+                        width: 200,
+                      )
                     : Container(
-                  height: 200.0,
-                  width: 200.0,
-                  color: Colors.grey[300],
-                  child: Icon(Icons.photo_camera,
-                      color: Colors.white, size: 50.0),
-                ),
+                        height: 200.0,
+                        width: 200.0,
+                        color: Colors.grey[300],
+                        child: Icon(Icons.photo_camera,
+                            color: Colors.white, size: 50.0),
+                      ),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal:20.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
             child: Text('I am...',
                 // Text placement will change depend on the search result
                 textAlign: TextAlign.left,
@@ -159,8 +159,8 @@ class _BodyState extends State<Body> {
                       onPressed: (int newIndex) {
                         setState(() {
                           for (var buttonIndex = 0;
-                          buttonIndex < isselected.length;
-                          buttonIndex++) {
+                              buttonIndex < isselected.length;
+                              buttonIndex++) {
                             if (buttonIndex == newIndex) {
                               isselected[buttonIndex] = true;
                             } else {
@@ -175,14 +175,13 @@ class _BodyState extends State<Body> {
               ]),
           Align(
               alignment: Alignment(-0.8, 0),
-              child: Text(
-                  'a', // Text placement will change depend on the search result
+              child: Text('a',
+                  // Text placement will change depend on the search result
                   textAlign: TextAlign.left,
                   style: TextStyle(
                       color: Colors.grey,
                       fontSize: 35,
                       fontWeight: FontWeight.bold))),
-
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
             child: Container(
@@ -252,7 +251,7 @@ class _BodyState extends State<Body> {
                     controller: descriptionController,
                     decoration: InputDecoration(
                         contentPadding:
-                        const EdgeInsets.fromLTRB(10, 40, 10, 40),
+                            const EdgeInsets.fromLTRB(10, 40, 10, 40),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -284,7 +283,7 @@ class _BodyState extends State<Body> {
                           language: 'en');
                       if (_prediction != null) {
                         var _selected =
-                        await _mapHandler.getLatLng(_prediction);
+                            await _mapHandler.getLatLng(_prediction);
 
                         setState(() {
                           _currentLocation = _selected;
@@ -294,7 +293,7 @@ class _BodyState extends State<Body> {
                     },
                     decoration: InputDecoration(
                         contentPadding:
-                        const EdgeInsets.fromLTRB(10, 30, 10, 30),
+                            const EdgeInsets.fromLTRB(10, 30, 10, 30),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -302,7 +301,6 @@ class _BodyState extends State<Body> {
               ),
             ),
           ),
-
           SizedBox(height: 20),
           Container(
             padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
@@ -313,46 +311,108 @@ class _BodyState extends State<Body> {
               // width of the button
               height: 50,
               onPressed: () async {
-                if (listingTitleController.text == '') {
-                  print('No listing title inputted');
-                  return; //TODO frontend user warning for empty listingTitle/itemName
+                // validation
+                if (image == null) {
+                  await Dialogs.errorAbortDialog(
+                      context, 'Please select a photo!');
+                  print('No image uploaded');
+                  return;
                 }
 
                 if (valueChoose == null) {
+                  final action = await Dialogs.errorAbortDialog(
+                      context, 'Please select a category!');
                   print('No category selected');
-                  return; //TODO frontend user warning for unselected category
+                  return;
                 }
 
-                if (image == null) {
-                  print('No image uploaded');
-                  return; //TODO frontend user warning for no uploaded image
+                if (listingTitleController.text == '') {
+                  final action = await Dialogs.errorAbortDialog(
+                      context, 'Name of product cannot be empty!');
+                  print('No listing title inputted');
+                  return;
                 }
 
                 if (_currentLocation == null) {
+                  await Dialogs.errorAbortDialog(
+                      context, 'Address cannot be empty!\n');
                   print('No location selected');
-                  return; //TODO frontend user warning no unselected location
+                  return;
                 }
 
+                // upload image
                 var imageString = await storageAccess.uploadFile(image);
 
-                var newListing = Listing(
-                  userID: userid,
-                  listingTitle: listingTitleController.text,
-                  longitude: _currentLocation.longitude,
-                  latitude: _currentLocation.latitude,
-                  category: valueChoose,
-                  isRequest: isselected[1],
-                  listingImage: imageString,
-                  isComplete: false,
-                  description: descriptionController.text,
-                );
+                var mp = MatchPresenter();
+                var classifications = await mp.fetchCategories(imageString);
+                var matches;
 
-                await dao.addListing(newListing);
-                //execute update
-                // Navigator.of(context).pushReplacement(
-                //     MaterialPageRoute(builder: (context) => Home()));
-                await Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => UserMain()));
+                // if it is a request listing, then try to do matches
+                if (isselected[1]) {
+                  matches = await mp.getMatches(classifications);
+                }
+
+                // if there are matches, display dialog
+                if (isselected[1] && matches!= null) {
+                  print(matches.length);
+                  print('At least one found!');
+
+                  await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return matchesDialog(context, matches, () async {
+                          // user clicks on "Post Anyway"
+                          var listingID = await postListing(
+                            context,
+                            dao,
+                            userid,
+                            listingTitleController.text,
+                            _currentLocation.longitude,
+                            _currentLocation.latitude,
+                            valueChoose,
+                            isselected[1],
+                            imageString,
+                            descriptionController.text,
+                          );
+
+                          // close popup
+                          await Navigator.of(context).pop();
+
+                          // push replace home page
+                          await Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => UserMain()));
+                          return;
+                        });
+                      });
+                }else{
+                  // post listing
+                  await print('No matches, or not a Request listing!');
+                  var listingID = await postListing(
+                    context,
+                    dao,
+                    userid,
+                    listingTitleController.text,
+                    _currentLocation.longitude,
+                    _currentLocation.latitude,
+                    valueChoose,
+                    isselected[1],
+                    imageString,
+                    descriptionController.text,
+                  );
+
+                  // add image classification for this listing to db
+                  await print('we got thelisting!!!! $listingID');
+                  var temp = await ListingImageData(listingID: listingID, categories: classifications);
+                  await print('LID: ${temp.listingID}, ${temp.categories}');
+                  await mp.addListingImageData(temp);
+
+                  // push replace home page
+                  await Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => UserMain()));
+                }
+
               },
               color: Color(0xFFFFC857),
               shape: RoundedRectangleBorder(
@@ -362,8 +422,6 @@ class _BodyState extends State<Body> {
                   style: TextStyle(color: Colors.grey[600], fontSize: 35)),
             ),
           ),
-
-          //////////////////////////////////////////////////////////////////////////////////////
         ]),
       ),
     );
@@ -431,7 +489,7 @@ class _BodyState extends State<Body> {
 
 // builds the pop dialog
 // TODO: need to pass in data of the matched listings
-Widget matchesDialog(context) {
+Widget matchesDialog(context, List<Listing> matches, postanyway) {
   return AlertDialog(
       title: Text('Hold on! We found some matches!'),
       content: Container(
@@ -439,19 +497,17 @@ Widget matchesDialog(context) {
         width: 300.0, // Change as per your requirement
         child: ListView.builder(
           shrinkWrap: true,
-          itemCount: 5,
+          itemCount: matches.length,
           itemBuilder: (BuildContext context, int index) {
-            return matchCard(
-                'Match!',
-                'This is a test description of the food!!',
-                'assets/noodles.jpg', () {
+            return matchCard(matches[index].listingTitle,
+                matches[index].description, matches[index].listingImage, () {
               print('tapped');
             });
           },
         ),
       ),
       actions: [
-        TextButton(child: Text('Post Anyway'), onPressed: () {}),
+        TextButton(child: Text('Post Anyway'), onPressed: postanyway),
         TextButton(
             child: Text('Cancel'),
             onPressed: () {
@@ -469,7 +525,7 @@ Widget matchCard(String title, String description, String listingImg, onTap) {
         child: ListTile(
           leading: CircleAvatar(
             radius: 30,
-            backgroundImage: AssetImage(
+            backgroundImage: NetworkImage(
                 listingImg), // no matter how big it is, it won't overflow
           ),
           contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
@@ -487,4 +543,24 @@ Widget matchCard(String title, String description, String listingImg, onTap) {
           trailing: Icon(Icons.keyboard_arrow_right),
         )),
   );
+}
+
+Future<String> postListing(context, dao, userid, title, loc_long, loc_lat, cat,
+    isselected, imageString, description) async {
+  // create listing
+  var newListing = await Listing(
+    userID: userid,
+    listingTitle: title,
+    longitude: loc_long,
+    latitude: loc_lat,
+    category: cat,
+    isRequest: isselected,
+    listingImage: imageString,
+    isComplete: false,
+    description: description,
+  );
+
+  var listingID = await dao.addListing(newListing);
+  return listingID;
+
 }
